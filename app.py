80279748fee9dd17e5f9a715a6f221da9810b0e4
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-01-18 22:45:40 +0530

    included health check in API

--- app.py
+++ app.py
@@ -22,10 +22,15 @@ class telegram(Resource):
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
         requests.get(url).json() # this sends the message
         return jsonify({'message':'message sent on telegram'})
-    
+
+class check(Resource):
+    def get(self):
+        return jsonify({'result':'Service is live!'})  
+      
 api.add_resource(telegram, '/') 
+api.add_resource(check, '/check')
 
 if __name__ == '__main__': 
   
-    #app.run(debug = True, host='127.1.1.2', port=8081) #this is for local dev
-    app.run(debug = True) #this is for cloud run
+    app.run(debug = True, host='127.1.1.2', port=8081) #this is for local dev
+    #app.run(debug = True) #this is for cloud run
