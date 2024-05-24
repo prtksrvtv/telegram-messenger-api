6f621a6d52e3a8892267bb025e0af48a44d9222d
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-05-24 15:01:17 +0530

    updated to integrate with react app

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import json
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api 
+from flask_cors import CORS
 
 #loading env variables
 load_dotenv()
 # creating the flask app 
 app = Flask(__name__) 
+CORS(app)
 # creating an API object 
 api = Api(app) 
 
@@ -16,7 +18,7 @@ class telegram(Resource):
 
     def post(self):
         message=request.get_json()
-        message=json.loads(message)
+        message=message['message']      
         TOKEN = os.environ['TOKEN']
         chat_id = os.environ['CHAT_ID']
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
@@ -25,12 +27,12 @@ class telegram(Resource):
 
 class check(Resource):
     def get(self):
-        return jsonify({'result':' Telegram API Service is live!'})  
+        return jsonify({'status': 200})  
       
 api.add_resource(telegram, '/') 
 api.add_resource(check, '/check')
 
 if __name__ == '__main__': 
   
-    #app.run(debug = True, host='127.1.1.2', port=8081) #this is for local dev
-    app.run() #this is for cloud run
+    app.run(debug = True, host='127.1.1.2', port=8081) #this is for local dev
+    #app.run() #this is for cloud run
