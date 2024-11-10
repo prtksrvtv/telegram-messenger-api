00fb24a179454bcb9bfe63942a3c98e2fa440ccc
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-11-10 23:48:23 +0530

    added 200 status code

--- app.py
+++ app.py
@@ -18,16 +18,15 @@ class telegram(Resource):
     def post(self):
         message=request.get_json()
         message=message['message']
-        print(message)
         TOKEN = os.environ['TOKEN']
         chat_id = os.environ['CHAT_ID']
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
         requests.get(url).json() # this sends the message
-        return jsonify({'message':message})
+        return jsonify({'message':message}), 200
 
 class check(Resource):
     def get(self):
-        return jsonify({'status': 200})  
+        return jsonify({'status': 200}), 200
       
 api.add_resource(telegram, '/') 
 api.add_resource(check, '/check')
