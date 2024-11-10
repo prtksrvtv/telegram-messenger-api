de2c951d193d64899724e418a1efa296f2ce75be
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-11-10 21:24:57 +0530

    testing commit 1

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ class telegram(Resource):
 
     def post(self):
         message=request.get_json()
-        message=message['message']      
+        message=message['message']
+        print(message)
         TOKEN = os.environ['TOKEN']
         chat_id = os.environ['CHAT_ID']
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
         requests.get(url).json() # this sends the message
-        return jsonify({'message':'message sent on telegram'})
+        return jsonify({'message':message})
 
 class check(Resource):
     def get(self):
@@ -33,5 +34,5 @@ api.add_resource(check, '/check')
 
 if __name__ == '__main__': 
   
-    #app.run(debug = True, host='127.1.1.2', port=8081) #this is for local dev
+    #app.run(debug = True, host='localhost', port=5000) #this is for local dev
     app.run() #this is for cloud run
